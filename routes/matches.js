ff4629e1a402e02a77adbb9f9c661b477f24a373
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2020-07-22 01:14:53 -0400

    Fix delete routes

--- routes/matches.js
+++ routes/matches.js
@@ -7,6 +7,6 @@ router.get("/matches", controllers.getMatches);
 router.get("/matches/:id", controllers.getMatch);
 router.post("/matches", controllers.addMatch);
 router.put("/matches/:id", controllers.updateMatch);
-router.delete("matches/:id", controllers.deleteMatch);
+router.delete("/matches/:id", controllers.deleteMatch);
 
 module.exports = router;
--- routes/wrestlers.js
+++ routes/wrestlers.js
@@ -7,6 +7,6 @@ router.get("/wrestlers", controllers.getWrestlers);
 router.get("/wrestlers/:id", controllers.getWrestler);
 router.post("/wrestlers", controllers.addWrestler);
 router.put("/wrestlers/:id", controllers.updateWrestler);
-router.delete("wrestlers/:id", controllers.deleteWrestler);
+router.delete("/wrestlers/:id", controllers.deleteWrestler);
 
 module.exports = router;
