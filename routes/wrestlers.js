949d3fd40f2826dff705afc35750f404a9a44cb2
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2020-07-22 01:07:58 -0400

    Fixed route error for wrestlers

--- routes/wrestlers.js
+++ routes/wrestlers.js
@@ -9,4 +9,4 @@ router.post("/wrestlers", controllers.addWrestler);
 router.put("/wrestlers/:id", controllers.updateWrestler);
 router.delete("wrestlers/:id", controllers.deleteWrestler);
 
-module.exports = wrestler;
+module.exports = router;
