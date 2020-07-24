b7bc27252c7097a3b8ea2db7d9b365ae141014e0
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2020-07-24 11:11:23 -0400

    Added CORS requirement

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const router = require("./routes");
 
@@ -7,9 +8,27 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
 app.use("/", router);
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+//   app.options("*", (req, res) => {
+//     // allowed XHR methods
+//     res.header(
+//       "Access-Control-Allow-Methods",
+//       "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+//     );
+//     res.send();
+//   });
+// });
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
